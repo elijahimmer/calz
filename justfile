2d82aa4b2e7518fc7cb1fe7f4e4930b834c1e54d
Elijah M. Immer <me@eimmer.me>
2024-05-18 11:42:52 -0700

    Implement backspace and delete

---
+++ justfile
@@ -0,0 +1,3 @@
+update:
+    zig fetch --save git+https://github.com/elijahimmer/termi/#HEAD
+
--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,11 @@
 pub fn main() !void {
     const stdout_file = std.io.getStdOut();
     var bw = std.io.bufferedWriter(stdout_file.writer());
-    defer bw.flush() catch {};
     const stdout = bw.writer();
 
     const stdin_file = std.io.getStdIn();
-    //var br = std.io.bufferedReader(stdin_file.reader());
-    //const stdin = br.reader();
+    var br = std.io.bufferedReader(stdin_file.reader());
+    const stdin = br.reader();
 
     // set terminal mode
     var term_manager = try TermManager.init(stdout_file, stdin_file);
@@ -24,38 +23,94 @@ pub fn main() !void {
         .keys_as_escape_codes = true,
         .associated_text = true,
     });
-    try term_manager.sendCommand(.home, .{});
+    try Command.home.print(stdout, .{});
 
-    while (true) {
-        const input = try Input.parse.readOneInput(stdin_file.reader());
-
-        switch (input.input_type) {
-            .release => continue,
-            else => {},
-        }
-
-        try input.print(stdout);
-
-        try stdout.print("\t{s}   \tchord: '", .{@tagName(input.input_type)});
-
-        for (input.chord.constSlice()) |c| try print_ascii(stdout, c);
-
-        try stdout.writeAll("'\r\n");
+    main_loop: while (true) {
+        try stdout.print(">>> ", .{});
         try bw.flush();
 
-        if (@as(Input.KeyCodeTag, input.key_code) == .text and input.key_code.text == 'c' and input.modifiers.onlyActive(.ctrl)) break;
-        //// test panic
-        //if (@as(Input.KeyCodeTag, input.key_code) == .text and input.key_code.text == 'z' and input.modifiers.onlyActive(.ctrl)) @panic("test panic");
+        const line_raw = readLine(stdout_file.writer(), stdin) catch |err| {
+            if (err == error.ctrl_c) {
+                try stdout.print("\r\ntype ':q' to exit\r\n", .{});
+            } else log.err("{s}", .{@errorName(err)});
+
+            continue :main_loop;
+        };
+
+        const line = mem.trim(u8, line_raw.constSlice(), &std.ascii.whitespace);
+
+        if (mem.eql(u8, line, ":q")) break :main_loop;
+
+        try stdout.print("\r\n   '{s}' :: line\r\n", .{line});
     }
 }
 
-pub fn print_ascii(writer: anytype, char: u8) @TypeOf(writer).Error!void {
-    switch (char) {
-        // 1 => "^A", 2 => "^B", 3 => "^C", etc
-        0...31 => try writer.print("^{c}", .{'A' - 1 + char}),
-        127 => try writer.writeAll("Backspace"),
-        else => try writer.print("{c}", .{char}),
+const ReadLineError = error{
+    ctrl_c,
+};
+
+pub fn readLine(stdout: anytype, stdin: anytype) anyerror!BoundedArray(u8, line_length) {
+    var line = BoundedArray(u8, line_length){};
+    var cursor_pos: LinePos = 0;
+
+    read_loop: while (true) {
+        assert(cursor_pos <= line.len);
+
+        const input = try Input.parse.readOneInput(stdin);
+
+        if (input.input_type == .release) continue;
+
+        if (@as(Input.KeyCodeTag, input.key_code) == .text and input.key_code.text == 'c' and input.modifiers.onlyActive(.ctrl)) return ReadLineError.ctrl_c;
+        switch (input.key_code) {
+            .text => |text| {
+                if (cursor_pos == line.len) {
+                    try line.append(text);
+                    try stdout.writeByte(text);
+                    cursor_pos +|= 1;
+                } else {
+                    try line.insert(cursor_pos, text);
+
+                    const line_to_print = line.constSlice()[cursor_pos..];
+
+                    try stdout.print("{s}", .{line_to_print});
+                    try Command.move_left.print(stdout, .{line_to_print.len - 1});
+                    cursor_pos +|= 1;
+                }
+            },
+            .special => |special| switch (special) {
+                .right => {
+                    cursor_pos +|= 1;
+                    if (cursor_pos > line.len) cursor_pos = line.len else {
+                        try Command.move_right.print(stdout, .{1});
+                    }
+                },
+                .left => {
+                    if (cursor_pos > 0) {
+                        cursor_pos -|= 1;
+                        try Command.move_left.print(stdout, .{1});
+                    }
+                },
+                .backspace, .delete => |spec| delete: {
+                    if (spec == .backspace) {
+                        if (line.len == 0 or cursor_pos == 0) break :delete;
+
+                        cursor_pos -|= 1;
+                        try Command.move_left.print(stdout, .{1});
+                    } else if (spec == .delete and cursor_pos >= line.len) break :delete;
+
+                    _ = line.orderedRemove(cursor_pos);
+                    const line_to_print = line.constSlice()[cursor_pos..];
+                    try stdout.print("{s} ", .{line_to_print});
+                    try Command.move_left.print(stdout, .{line_to_print.len + 1});
+                },
+                .enter => break :read_loop,
+                else => {},
+            },
+            .unknown => {},
+        }
     }
+
+    return line;
 }
 
 pub fn panic(message: []const u8, error_return_trace: ?*std.builtin.StackTrace, ret_addr: ?usize) noreturn {
@@ -85,12 +140,20 @@ pub fn logFn(comptime message_level: std.log.Level, comptime scope: @TypeOf(.enu
 
 var global_term_manager: ?*TermManager = null;
 
+const LinePos = u8;
+const line_length = std.math.maxInt(LinePos);
+
 const termi = @import("termi");
 const Input = termi.Input;
 const chars = termi.chars;
 const ProgressiveEnhancement = termi.ProgressiveEnhancement;
 const TermManager = termi.TermManager;
+const Command = termi.Command;
 
 const std = @import("std");
 const posix = std.posix;
 const meta = std.meta;
+const mem = std.mem;
+const assert = std.debug.assert;
+const BoundedArray = std.BoundedArray;
+const log = std.log.scoped(.calz);
